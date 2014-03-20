b5b0b9dd0e4c930f1e4cf17afbc5775a85210b0b
javi <qualopec@gmail.com>
2014-03-20 09:27:46 +0100

    missing files

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ e.Actions = require('./lib/odyssey/actions');
 e.Triggers = require('./lib/odyssey/triggers');
 e.Core = require('./lib/odyssey/core');
 e.Template = require('./lib/odyssey/template');
+
+for (var k in e.Actions) {
+  e[k] = e.Actions[k];
+}
+for (var k in e.Triggers) {
+  e[k] = e.Triggers[k];
+}
 module.exports = e;
