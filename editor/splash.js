dac6cc78073f8b15dcfac1b12ff7083544fc2396
Emilio <piensaenpixel@gmail.com>
2014-05-21 17:17:39 +0200

    add splash page

--- editor/splash.js
+++ editor/splash.js
@@ -8,18 +8,25 @@ function Splash(context) {
       .selectAll('.splash')
       .data([0]);
 
-    var div = s.enter().append('div').attr('class', 'splash');
-    div.append('h1').text('Select your template')
-    div.append('p').text('templates allow you to change the layout of the styory')
-    var templates = div.append('div').attr('class', 'template_list')
+    var div = s.enter().append('div').attr('class', 'splash h-valign');
+
+    var inner_content = div.append('div').attr('class', 'splash_inner')
+
+    inner_content.append('h1').text('Select your template')
+    inner_content.append('p').text('templates allow you to change the layout of the styory')
+    var templates = inner_content.append('ul').attr('class', 'template_list h-valign')
+
+
     var template = templates 
       .selectAll('.template').data(context.templates())
       .enter()
-        .append('div')
-        .attr('class', 'template')
+        .append('li')
+        .attr('class', 'template h-valign')
+        .append ('div')
+        .attr('class', 'inner-template')
 
     template
-        .append('h1')
+        .append('h2')
           .text(function(d) {
             return d.title
           })
@@ -28,6 +35,10 @@ function Splash(context) {
           .text(function(d) {
             return d.description
           })
+
+    template
+        .append('a').text('SELECT').attr('class', 'button-template')
+
 
     template.on('click', function(d) {
       evt.template(d.title);
