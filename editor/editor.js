5e0793fcab975f4088050088c39dba6a87b42790
Carlos Matallín <matallo@vizzuality.com>
2014-06-04 13:31:17 +0200

    update default time

--- editor/editor.js
+++ editor/editor.js
@@ -20,7 +20,7 @@ var TEMPLATE_LIST =  [{
   }, {
     title: 'torque',
     description: 'Create a visualization that changes as your reader moves through your narrative',
-    default: '```\n-title: "Title"\n-author: "Name"\n-vizjson: "http://viz2.cartodb.com/api/v2/viz/521f3768-eb3c-11e3-b456-0e10bcd91c2b/viz.json"\n-duration: "512"\n```\n\n#title\n##headline\n\n#slide1\nsome text\n\n#slide2\n more text'
+    default: '```\n-title: "Title"\n-author: "Name"\n-vizjson: "http://viz2.cartodb.com/api/v2/viz/521f3768-eb3c-11e3-b456-0e10bcd91c2b/viz.json"\n-duration: 30\n```\n\n#title\n##headline\n\n#slide1\nsome text\n\n#slide2\n more text'
   }
 ];
 
