41e54cff7acbb580397bc02f8305bca86b7a3ede
Carlos Matallín <matallo@vizzuality.com>
2014-06-05 16:24:14 +0200

    update dist files

--- dist/editor.js
+++ dist/editor.js
@@ -133,7 +133,7 @@ function dialog(context) {
       .text(templates[0])
       .attr('href', '/odyssey.js/editor/editor.html')
       .on('click', function(d) {
-        d3.event.stopPropagation();
+        d3.event.preventDefault();
         d3.select(document.body).call(Splash(context).on('template', function(t) {
           evt.template(t);
         }));
@@ -391,13 +391,20 @@ var utils = _dereq_('./utils');
 var TEMPLATE_LIST =  [{
     title: 'slides',
     description: 'Display visualization chapters like slides in a presentation',
-    default: '```\n-title: "Title"\n-author: "Name"\n```\n\n#slide1\nsome text\n\n#slide2\n more text'
+    default: '```\n-title: "Title"\n-author: "Name"\n```\n\n#slide1\nsome text\n\n#slide2\nmore text'
   }, {
     title: 'scroll',
     description: 'Create a visualization that changes as your reader moves through your narrative',
-    default: '```\n-title: "Title"\n-author: "Name"\n```\n\n#title\n##headline\n\n#slide1\nsome text\n\n#slide2\n more text'
+    default: '```\n-title: "Title"\n-author: "Name"\n```\n\n#title\n##headline\n\n#slide1\nsome text\n\n\n#slide2\nmore text'
+  }, {
+    title: 'torque',
+    description: 'Create a visualization that changes as your reader moves through your narrative',
+    default: '```\n-title: "Title"\n-author: "Name"\n-vizjson: "http://viz2.cartodb.com/api/v2/viz/521f3768-eb3c-11e3-b456-0e10bcd91c2b/viz.json"\n-duration: 30\n```\n\n#slide1\n```\n-step: 100\n```\nsome text\n\n\n#slide2\n```\n-step: 200\n```\nmore text'
   }
 ];
+
+
+
 
 
 function editor() {
@@ -801,18 +808,8 @@ function debounce(fn, t, ctx) {
   }
 }
 
-function set_template(t) {
-  var html_url = t + ".html";
-  if (template.attr('src') !== html_url) {
-    template.attr('src', t + ".html");
-    context.template(t);
-    context.save();
-  }
-}
-
 module.exports = {
-  debounce: debounce,
-  set_template: set_template
+  debounce: debounce
 }
 
 },{}],8:[function(_dereq_,module,exports){
